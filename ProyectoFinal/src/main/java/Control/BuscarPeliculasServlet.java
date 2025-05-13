5c9eb66f882597269a52641fd3781efcc0de6f79
superQ78 <167489927+superQ78@users.noreply.github.com>
2025-05-13 12:58:33 -0700

    Servlet2

--- ProyectoFinal/src/main/java/Control/BuscarPeliculasServlet.java
+++ ProyectoFinal/src/main/java/Control/BuscarPeliculasServlet.java
@@ -4,10 +4,33 @@
  */
 package Control;
 
+import DTO.PeliculaDTO;
+import Negocio.PeliculaBO;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  *
  * @author cesar
  */
-public class BuscarPeliculasServlet {
-    
+@WebServlet("/BuscarPeliculasServlet")
+public class BuscarPeliculasServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String titulo = request.getParameter("titulo");
+        String usuarioId = (String) request.getSession().getAttribute("usuarioId"); // 🔹 Usa tu nombre de atributo real
+
+        PeliculaBO peliculaBO = new PeliculaBO();
+        List<PeliculaDTO> peliculasEncontradas = peliculaBO.buscarPorTitulo(titulo, usuarioId);
+
+        request.setAttribute("peliculas", peliculasEncontradas);
+        request.setAttribute("busqueda", titulo);
+        request.getRequestDispatcher("ResultadosBusqueda.jsp").forward(request, response);
+    }
 }
